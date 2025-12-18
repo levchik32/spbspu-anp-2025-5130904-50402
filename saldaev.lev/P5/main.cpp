89d313b54767f325440a19723c9888560f59f1f1
Carrot_lord <lev.saldaev@gmai.com>
2025-12-18 19:59:51 +0300

    add base shape structs and interface

--- saldaev.lev/P5/main.cpp
+++ saldaev.lev/P5/main.cpp
@@ -1,4 +1,28 @@
+namespace saldaev
+{
+	struct Point_t
+	{
+		double x, y;
+	};
+
+	struct Rectangle_t
+	{
+		double width, height;
+		Point_t pos;
+	};
+
+	struct Shape
+	{
+		virtual double getArea() = 0;
+		virtual Rectangle_t getFrameRect() = 0;
+		virtual void move(Point_t target) = 0;
+		virtual void move(double dx, double dy) = 0;
+		virtual void scale(double coefficient) = 0;
+		virtual ~Shape() = default;
+	};
+	
+}
+
 int main()
 {
-    
 }
