253f9e7d67414ba7bc779dfe46fe4c5dfac08ece
Carrot_lord <lev.saldaev@gmai.com>
2025-12-19 00:58:32 +0300

    Implement Polygon struct with rule of five

--- saldaev.lev/P5/main.cpp
+++ saldaev.lev/P5/main.cpp
@@ -38,6 +38,29 @@ namespace saldaev
 		Point_t pos;
 		double side;
 	};
+
+	struct Polygon : Shape
+	{
+		Polygon(Point_t *vs, size_t kk);
+		~Polygon();
+		Polygon(const Polygon &other);
+		Polygon &operator=(const Polygon &other);
+		Polygon(Polygon &&other) noexcept;
+		Polygon &operator=(Polygon &&other) noexcept;
+
+		double getArea() const override;
+		Rectangle_t getFrameRect() const override;
+		void move(Point_t target) override;
+		void move(double dx, double dy) override;
+		void scale(double coef) override;
+
+	private:
+		Point_t *vertexes;
+		size_t k;
+		Point_t pos;
+	};
+
+	Point_t calculateCenter(Point_t *vs, size_t kk);
 }
 
 int main()
@@ -45,14 +68,14 @@ int main()
 }
 
 //
-saldaev::Point_t & saldaev::Point_t::operator-=(const saldaev::Point_t &other)
+saldaev::Point_t &saldaev::Point_t::operator-=(const Point_t &other)
 {
 	x -= other.x;
 	y -= other.y;
 	return *this;
 }
 
-saldaev::Point_t & saldaev::Point_t::operator*=(const double &other)
+saldaev::Point_t &saldaev::Point_t::operator*=(const double &other)
 {
 	x *= other;
 	y *= other;
@@ -88,4 +111,73 @@ void saldaev::Square::move(double dx, double dy)
 void saldaev::Square::scale(double coef)
 {
 	side *= coef;
+}
+
+//
+saldaev::Polygon::Polygon(Point_t *vs, size_t kk) : k(kk), pos(calculateCenter(vs, kk))
+{
+	vertexes = new Point_t[k];
+	for (size_t i = 0; i < k; ++i)
+	{
+		vertexes[i] = vs[i];
+	}
+}
+
+saldaev::Polygon::~Polygon()
+{
+	delete[] vertexes;
+}
+
+saldaev::Polygon::Polygon(const Polygon &other) : k(other.k),
+																									vertexes(new Point_t[other.k]),
+																									pos(other.pos)
+{
+	for (size_t i = 0; i < k; ++i)
+	{
+		vertexes[i] = other.vertexes[i];
+	}
+}
+
+saldaev::Polygon &saldaev::Polygon::operator=(const Polygon &other)
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+
+	k = other.k;
+	pos = other.pos;
+
+	delete[] vertexes;
+	vertexes = new Point_t[other.k];
+	for (size_t i = 0; i < k; ++i)
+	{
+		vertexes[i] = other.vertexes[i];
+	}
+
+	return *this;
+}
+
+saldaev::Polygon::Polygon(Polygon &&other) noexcept : k(other.k),
+																											vertexes(other.vertexes),
+																											pos(other.pos)
+{
+	other.vertexes = nullptr;
+}
+
+saldaev::Polygon &saldaev::Polygon::operator=(Polygon &&other) noexcept
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+
+	k = other.k;
+	pos = other.pos;
+
+	delete[] vertexes;
+	vertexes = other.vertexes;
+	other.vertexes = nullptr;
+
+	return *this;
 }
